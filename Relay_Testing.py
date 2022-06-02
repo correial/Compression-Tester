05d66977fbe9f5a09a50286ea65459d8e0891b58
Lucca Correia <luccacorreia@Luccas-MBP-2.fios-router.home>
2022-06-01 21:36:30 -0400

    comments

--- Relay_Testing.py
+++ Relay_Testing.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+# turns linear actuator off
 def off():
     ("Status: OFF")
     RELAIS_1_GPIO = 22
@@ -10,7 +11,7 @@ def off():
     GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
     sleep(.1)
 
-
+# turns linear actuator off
 def on():
     print ("Status: ON, IDLE")
     RELAIS_1_GPIO = 22
@@ -18,19 +19,21 @@ def on():
     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
     sleep(1)
 
+# extends linear actuator arm
 def out():
     print ("Status: ON, OUT")
     RELAIS_1_GPIO = 17
     GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
-    sleep(5)
+    sleep(5) #time in which actuator arm is moving
 
+# retracts linear actuator arm 
 def intake():
     print ("Status: ON, IN")
     RELAIS_1_GPIO = 17
     GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
     GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
-    sleep(5)
+    sleep(5) #time in which actuator arm is moving
 
 def load_cell():
     X = 0
@@ -39,7 +42,16 @@ def load_cell():
     X = GPIO.input(25)
     print(X)
 
+'''
+Here will be the while statement to stop the load cell at a specific threshold of force. 
+The value (val) come from the hx711 exampl.py file
 
-on()
-intake()
+example: 
+
+while val <= 50
+    on()
+    outake()
+
 off()
+
+'''
